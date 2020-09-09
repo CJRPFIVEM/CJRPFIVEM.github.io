d7f07302a459e2e99b79a4cdf7e9d6526bb0d6ba
CJRPFIVEM <68623051+CJRPFIVEM@users.noreply.github.com>
2020-09-09 18:04:55 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <title></title>
+        <!--<link rel="stylesheet" href="style.css">--> 
+        </head>
+        <body>
+
+            <header>
+                <nav>
+                <a href="#">
+                    <img src="CJRP.png" alt="CJRP">
+                </a>
+                <ul>
+                    <li>a href="home">Home</a></li>
+                    <li>a href="updates">Updates</a></li>
+                    <li>a href="contact">Contact</a></li>
+                    <li>a href="about">About</a></li>
+                    <li>a href="media">Media</a></li>
+                    <li>a href="join">Join</a></li>
+                    </ul>
+                    <div>
+                        <form class="" action="login.inc.php" method="post">
+                            <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                            <input type="password" name="pwd" placeholder="Password...">
+                            <button type="submit" name="login-submit">Login</button>
+                            </form>
+                            <a href="signup.php">Signup</a>
+                            <form action="logout.inc.php" method="post">
+                            <button type="submit" name="logout-submit">Logout</button>
+                            ></form>
+                           </div>
+                          </nav>  
+                </nav>
+
+            </header>
