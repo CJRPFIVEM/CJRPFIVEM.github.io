0f0bc6145a6a6d660fa5dbc3dadf17bf112256ee
CJRPFIVEM <68623051+CJRPFIVEM@users.noreply.github.com>
2020-09-09 18:51:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// DOM elements
+const guideList = document.querySelector('.guides');
+
+// setup guides
+const setupGuides = (data) => {
+
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html
+  } else {
+    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
+  }
+  
+
+};
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
