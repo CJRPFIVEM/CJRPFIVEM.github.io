c4ab9c3c02e611815ac6e4794f75e417fde70615
CJRPFIVEM <68623051+CJRPFIVEM@users.noreply.github.com>
2020-09-17 22:42:38 +0100

    Update login.js

--- login.js
+++ login.js
@@ -5,3 +5,16 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'ux_mode': 'redirect',
+        'redirect_uri': 'https://cjrpfivem.github.io/home',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
